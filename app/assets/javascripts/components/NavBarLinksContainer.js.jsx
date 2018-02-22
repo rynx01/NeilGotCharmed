a465c5bc82fb34ca5894bf9403e6e36e9ec55017
Ryan <ryanjdayrit@intelimina.com>
2018-02-22 19:25:59 +0800

    Fixbug: links not properly working and added margin to icons

--- app/assets/javascripts/components/NavBarLinksContainer.js.jsx
+++ app/assets/javascripts/components/NavBarLinksContainer.js.jsx
@@ -1,7 +1,5 @@
 const NavBarLinksContainer  = ({ url, className, title }) => (
-    <li>
-        <a href={ url }>
-            <i className={ className }></i>{ title }
-        </a>
+    <li onClick={() => window.location = url }>
+        <a><i style={{ margin: `1rem` }} className={ className }></i>{ title }</a>
     </li>
 )
